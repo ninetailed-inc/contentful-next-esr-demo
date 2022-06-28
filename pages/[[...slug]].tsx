d0f2db86d90c2386fe0296cf737d321f02355ee0
maxrawlinger <max.rawlinger@googlemail.com>
2022-06-28 15:12:37 +0200

    feat: add esr client-side logic

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -2,12 +2,15 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import { NextSeo } from 'next-seo';
 import get from 'lodash/get';
 
+import { useProfile } from '@ninetailed/experience.js-next';
 import { BlockRenderer } from '@/components/Renderer';
 import { getPagesOfType, getPage } from '@/lib/api';
 import { PAGE_CONTENT_TYPES } from '@/lib/constants';
 import { IPage } from '@/types/contentful';
 
 const Page = ({ page }: { page: IPage }) => {
+  const { profile: userProfile } = useProfile();
+  console.log({ 'APP:UserProfile': userProfile });
   if (!page) {
     return null;
   }
@@ -38,8 +41,66 @@ const Page = ({ page }: { page: IPage }) => {
     </>
   );
 };
-
 export const getStaticProps: GetStaticProps = async ({ params, preview }) => {
+  console.log({ 'SLUG:params': params });
+
+  const rawSlug = get(params, 'slug', []) as string[];
+
+  console.log({ 'SLUG:rawSlug': rawSlug });
+
+  const audiencesSlug = rawSlug[0] || '';
+  const isPersonalized = audiencesSlug.startsWith(';');
+  const audiences = isPersonalized
+    ? audiencesSlug.split(';')[1].split(',')
+    : [];
+  const slug = isPersonalized ? rawSlug.slice(1).join('/') : rawSlug.join('/');
+  const page = await getPage({
+    preview,
+    slug: slug === '' ? '/' : slug,
+    pageContentType: PAGE_CONTENT_TYPES.PAGE,
+    childPageContentType: PAGE_CONTENT_TYPES.LANDING_PAGE,
+  });
+  /* console.log({ 'SLUG:page': page }); */
+  return {
+    props: { page, ninetailed: { audiences } },
+    revalidate: 5,
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const pages = await getPagesOfType({
+    pageContentType: PAGE_CONTENT_TYPES.PAGE,
+    childPageContentType: PAGE_CONTENT_TYPES.LANDING_PAGE,
+  });
+
+  const paths = pages
+    .filter((page) => {
+      return page.fields.slug !== '/';
+    })
+    .map((page) => {
+      return {
+        params: { slug: page.fields.slug.split('/') },
+      };
+    });
+  /* console.log({ 'SLUG:paths': JSON.stringify(paths) }); */
+  /* return {
+    paths: [
+      { params: { slug: [''] } },
+      { params: { slug: [';7IRVaTD9GpZVprP7A8tSiE', 'pricing'] } },
+    ],
+    fallback: true,
+  }; */
+  /* return {
+    paths: [{ params: { slug: [''] } }, { params: { slug: ['pricing'] } }],
+    fallback: true,
+  }; */
+  return {
+    paths: [...paths, { params: { slug: [''] } }],
+    fallback: true,
+  };
+};
+
+/* export const getStaticProps: GetStaticProps = async ({ params, preview }) => {
   const rawSlug = get(params, 'slug', []) as string[];
   const slug = rawSlug.join('/');
   const page = await getPage({
@@ -73,6 +134,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
     paths: [...paths, { params: { slug: [''] } }],
     fallback: true,
   };
-};
+}; */
 
 export default Page;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,68 @@
 import React from 'react';
 import '@/styles/globals.css';
 import { AppProps } from 'next/app';
-import { NinetailedProvider } from '@ninetailed/experience.js-next';
+import {
+  NinetailedProvider,
+  NinetailedSsrPlugin,
+  Profile,
+} from '@ninetailed/experience.js-next';
 import { NinetailedPreviewPlugin } from '@ninetailed/experience.js-plugin-preview';
+import Cookies from 'js-cookie';
+import { IPage } from '@/types/contentful';
 
-const B2BDemoApp = ({ Component, pageProps }: AppProps) => {
+export type IPageProps = {
+  page: IPage;
+  ninetailed: { audiences: string[] };
+};
+
+const ESRDemoApp = ({ Component, pageProps }: AppProps) => {
+  console.log('-------------_RENDER-----------------');
+  const { ninetailed, page } = pageProps as IPageProps;
+  console.log({ 'APP:PageProps': page });
+  const audiences = ninetailed?.audiences || [];
+  console.log({ 'APP:ninetailedProps': ninetailed });
+  console.log({ 'APP:audiences': audiences });
+  const profile = React.useMemo(() => {
+    const id = Cookies.get('ntaid') as string;
+    console.log({ 'APP:ntaid': id });
+    const defaultProfile: Profile = {
+      id,
+      random: 0,
+      audiences,
+      traits: {},
+      session: {
+        isReturningVisitor: false,
+        count: 0,
+        landingPage: {
+          path: '',
+          query: {},
+          referrer: '',
+          search: '',
+          url: '',
+        },
+        activeSessionLength: 0,
+        averageSessionLength: 0,
+      },
+      location: {},
+    };
+    return defaultProfile;
+  }, []);
+
+  console.log({ 'APP:pageProps': pageProps });
+  console.log({
+    'APP:NINETAILED_API_KEY': process.env.NEXT_PUBLIC_NINETAILED_API_KEY,
+  });
+  console.log({
+    'APP:NINETAILED_ENVIRONMENT':
+      process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT,
+  });
+
   return (
     <div className="app">
       <NinetailedProvider
+        profile={profile}
         plugins={[
+          NinetailedSsrPlugin(),
           NinetailedPreviewPlugin({
             clientId:
               process.env.NEXT_PUBLIC_NINETAILED_MANAGEMENT_CLIENT_ID ?? '',
@@ -27,4 +81,4 @@ const B2BDemoApp = ({ Component, pageProps }: AppProps) => {
   );
 };
 
-export default B2BDemoApp;
+export default ESRDemoApp;
